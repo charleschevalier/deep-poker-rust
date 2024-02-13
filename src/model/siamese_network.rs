aad9c3646b76cde4de454851e0946d8fdd8d9e6b
charles <chevalier.charles@gmail.com>
2024-02-13 01:25:59 +0100

    Model: trying to change siamese to MLP instead of CNN

--- src/model/siamese_network.rs
+++ src/model/siamese_network.rs
@@ -1,9 +1,9 @@
 use candle_core::{Module, Tensor};
-use candle_nn::{conv2d, linear, Conv2d, Conv2dConfig, Linear, VarBuilder};
+use candle_nn::{conv2d, linear, seq, Conv2d, Conv2dConfig, Linear, Sequential, VarBuilder};
 
 pub struct SiameseNetwork {
-    card_conv: Conv2d,
-    action_conv: Conv2d,
+    card_layer: Sequential,
+    action_layer: Sequential,
     merge_layer: Linear,
     output_layer: Linear,
 }
@@ -29,9 +29,10 @@ impl SiameseNetwork {
 
         // Define layers dimensions
         let weight_dims: Vec<Vec<usize>> = vec![
-            // Card convolution layer
+            // Card convolution layers
             vec![5 * 8, 5, 3, 3],
-            // Action convolution layer
+            vec![5 * 16, 5 * 8, 3, 3],
+            // Action convolution layers
             vec![
                 max_action_per_street_cnt * 4 * 8,
                 max_action_per_street_cnt * 4,
@@ -47,38 +48,27 @@ impl SiameseNetwork {
         // Define card convolution layer
         // Card input shape: 5 channels for hole cards, 3 streets, all cards
         // 52 cards: 13 ranks, 4 suits
-        let card_conv = conv2d(
-            weight_dims[0][1],
-            weight_dims[0][0],
-            weight_dims[0][2],
-            Conv2dConfig::default(),
-            vb.pp("siamese_card_conv_1"),
-        )?;
+        let card_layer = seq()
+            .add(linear(5 * 4 * 13, 512, vb.pp("card_1"))?)
+            .add(linear(512, 512, vb.pp("card_2"))?);
 
         // Define action convolution layer
         // Action input shape: 4 streets with max_action_per_street_cnt actions each
         // action_abstraction_count possible actions
         // player_count + 2 channels for sum and legal actions
-        let action_conv = conv2d(
-            weight_dims[1][1],
-            weight_dims[1][0],
-            weight_dims[1][2],
-            Conv2dConfig::default(),
-            vb.pp("siamese_action_conv_1"),
-        )?;
+        let action_layer = seq()
+            .add(linear(4 * 9 * 5 * 7, 512, vb.pp("action_1"))?)
+            .add(linear(512, 512, vb.pp("action_2"))?);
+
         // println!("Action conv shape: {:?}", action_conv.weight().shape());
 
-        let merge_layer = linear(weight_dims[2][1], weight_dims[2][0], vb.pp("siamese_merge"))?;
+        let merge_layer = linear(1024, 1024, vb.pp("siamese_merge"))?;
 
-        let output_layer = linear(
-            weight_dims[3][1],
-            weight_dims[3][0],
-            vb.pp("siamese_output"),
-        )?;
+        let output_layer = linear(1024, 256, vb.pp("siamese_output"))?;
 
         Ok(SiameseNetwork {
-            card_conv,
-            action_conv,
+            card_layer,
+            action_layer,
             merge_layer,
             output_layer,
         })
@@ -89,21 +79,24 @@ impl SiameseNetwork {
         card_tensor: &Tensor,
         action_tensor: &Tensor,
     ) -> Result<Tensor, candle_core::Error> {
-        let card_output = self.card_conv.forward(card_tensor)?;
+        // println!("card_tensor shape: {:?}", card_tensor.shape());
+        // println!("action_tensor shape: {:?}", action_tensor.shape());
+
+        let card_output = self.card_layer.forward(&card_tensor.flatten(1, 3)?)?;
 
         // println!("Card output shape: {:?}", card_output.shape());
 
-        let action_output = self.action_conv.forward(action_tensor)?;
+        let action_output = self.action_layer.forward(&action_tensor.flatten(1, 3)?)?;
 
         // println!("Action output shape: {:?}", action_output.shape());
 
-        let card_output_flat = card_output.flatten(1, 3)?;
-        let action_output_flat = action_output.flatten(1, 3)?;
+        // let card_output_flat = card_output.flatten(1, 3)?;
+        // let action_output_flat = action_output.flatten(1, 3)?;
 
         // println!("Card output flat shape: {:?}", card_output_flat.shape());
         // println!("Action output flat shape: {:?}", action_output_flat.shape());
 
-        let merged = Tensor::cat(&[&card_output_flat, &action_output_flat], 1)?;
+        let merged = Tensor::cat(&[&card_output, &action_output], 1)?;
 
         // println!("Merged shape: {:?}", merged.shape());
 
